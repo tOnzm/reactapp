9434506a2108f64aa1d64b83f013c35b9a248660
tOnzm <tonselaman@gmail.com>
2023-12-27 17:14:03 +0700

    Update 271266-1

--- App.js
+++ App.js
@@ -1,208 +1,176 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image, Button, Alert, TouchableOpacity } from 'react-native';
-import Header from './components/Header';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
+import Header from "./components/Header";
 
 export default function App() {
   return (
-    <View style={styles.container} >
+    <View style={styles.container}>
       <Header title="ScammerBET" />
       <StatusBar style="auto" />
 
-      <View style={styles.box}>
-        <Text style={styles.textTitle}>
-          Premier League
-        </Text>
+      <View>{renderMatch()}</View>
+    </View>
+  );
+}
+
+const renderMatch = () => {
+  const matches = [];
+
+  for (let i = 0; i < 5; i++) {
+    matches.push(
+      <View key={i} style={styles.box}>
+        <Text style={styles.textTitle}>Premier League</Text>
 
         <View style={styles.team}>
           <TouchableOpacity
             style={styles.button}
-            onPress={() => Alert.alert('เลือกได้ดี')}>
+            onPress={() => Alert.alert("เลือกได้ดี")}
+          >
             <Text style={styles.handicapText}>-2/2.5</Text>
             <Text style={styles.ButtonText}>0.75</Text>
-
           </TouchableOpacity>
           <View style={styles.teamData}>
             <Image
               style={styles.tinyLogo}
               source={{
-                uri: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/53/Arsenal_FC.svg/1200px-Arsenal_FC.svg.png',
+                uri: "https://upload.wikimedia.org/wikipedia/en/thumb/5/53/Arsenal_FC.svg/1200px-Arsenal_FC.svg.png",
               }}
             />
-            <Text style={styles.teamName}>
-              Arsenal
-            </Text>
+            <Text style={styles.teamName}>Arsenal</Text>
           </View>
           <View style={styles.score}>
-            <Text style={styles.textScore}>
-              2
-            </Text>
-            <Text style={styles.textScore}>
-              -
-            </Text>
-            <Text style={styles.textScore}>
-              0
-            </Text>
+            <Text style={styles.textScore}>18</Text>
+            <Text style={styles.textScore}>:</Text>
+            <Text style={styles.textScore}>00</Text>
           </View>
           <View>
             <Image
               style={styles.tinyLogo}
               source={{
-                uri: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/1200px-Manchester_United_FC_crest.svg.png',
+                uri: "https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/1200px-Manchester_United_FC_crest.svg.png",
               }}
             />
-            <Text style={styles.teamName}>
-              ManUTD
-            </Text>
+            <Text style={styles.teamName}>ManUTD</Text>
           </View>
           <TouchableOpacity
             style={styles.button}
-            onPress={() => Alert.alert('คิดใหม่ ให้คิดอีกที')}>
+            onPress={() => {
+              Alert.alert("แน่ใจนะ", "คิดดีแล้วใช่มั้ย?", [
+                {
+                  text: "ยกเลิก",
+                  style: "cancel",
+                },
+                {
+                  text: "แน่นอน",
+                  onPress: () => {
+                    Alert.alert("ถามจริ๊ง!", "ลองทบทวนดูอีกที", [
+                      {
+                        text: "ยกเลิก",
+                        style: "cancel",
+                      },
+                      {
+                        text: "มั่นใจ",
+                        onPress: () => {
+                          Alert.alert("ควาย");
+                        },
+                      },
+                    ])
+                  }
+                },
+              ]);
+            }
+          }>
             <Text style={styles.handicapText}>+2/2.5</Text>
             <Text style={styles.ButtonText}>5.0</Text>
           </TouchableOpacity>
         </View>
       </View>
-      <View style={styles.box}>
-        <Text style={styles.textTitle}>
-          News
-        </Text>
+    );
+  }
 
-        <View style={styles.news}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => Alert.alert('เลือกได้ดี')}>
-            <Text style={styles.handicapText}>-2/2.5</Text>
-            <Text style={styles.ButtonText}>0.75</Text>
-
-          </TouchableOpacity>
-          <View style={styles.teamData}>
-            <Image
-              style={styles.tinyLogo}
-              source={{
-                uri: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/53/Arsenal_FC.svg/1200px-Arsenal_FC.svg.png',
-              }}
-            />
-            <Text style={styles.teamName}>
-              Arsenal
-            </Text>
-          </View>
-          <View style={styles.score}>
-            <Text style={styles.textScore}>
-              2
-            </Text>
-            <Text style={styles.textScore}>
-              -
-            </Text>
-            <Text style={styles.textScore}>
-              0
-            </Text>
-          </View>
-          <View>
-            <Image
-              style={styles.tinyLogo}
-              source={{
-                uri: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/1200px-Manchester_United_FC_crest.svg.png',
-              }}
-            />
-            <Text style={styles.teamName}>
-              ManUTD
-            </Text>
-          </View>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => Alert.alert('คิดใหม่ ให้คิดอีกที')}>
-            <Text style={styles.handicapText}>+2/2.5</Text>
-            <Text style={styles.ButtonText}>5.0</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-     
-
-    </View>
-
-
-  );
-}
+  return matches;
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 0,
-    backgroundColor: '#e3eec0',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#e3eec0",
+    alignItems: "center",
+    justifyContent: "center",
   },
 
   box: {
-    width: '100%',
-    alignItems: 'center',
+    width: "100%",
+    alignItems: "center",
     padding: 10,
-
   },
   tinyLogo: {
-    alignItems: 'center',
+    alignItems: "center",
     width: 50,
     height: 50,
     padding: 10,
     marginHorizontal: 10,
-    resizeMode: 'contain',
-
+    resizeMode: "contain",
   },
   team: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginTop: 20,
-
   },
   button: {
-    backgroundColor: '#5e6745',
-    color: '#aeba89',
+    backgroundColor: "#5e6745",
+    color: "#aeba89",
     padding: 10,
     borderRadius: 100 / 20,
     width: 60,
-    alignItems: 'center',
+    alignItems: "center",
   },
   textTitle: {
-    color: '#5e6745',
+    color: "#5e6745",
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   ButtonText: {
-    color: '#ffffff',
-    fontWeight: 'bold',
+    color: "#ffffff",
+    fontWeight: "bold",
   },
   score: {
-    flexDirection: 'row',
+    flexDirection: "row",
     borderWidth: 1,
-    borderColor: '#aeba89',
+    borderColor: "#aeba89",
     padding: 5,
     borderRadius: 100 / 20,
-
+    backgroundColor: "#fff",
   },
   textScore: {
-    color: '#5e6745',
-    fontWeight: 'bold',
+    color: "#5e6745",
+    fontWeight: "bold",
     fontSize: 22,
-
   },
   teamName: {
-    textAlign: 'center',
-    fontWeight: 'bold',
-    color: '#5e6745',
+    textAlign: "center",
+    fontWeight: "bold",
+    color: "#5e6745",
     fontSize: 12,
   },
 
-  teamData: {
-  },
+  teamData: {},
   handicapText: {
     fontSize: 12,
-    color: '#fff',
+    color: "#fff",
   },
   news: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginTop: 20,
-    
   },
-
 });
